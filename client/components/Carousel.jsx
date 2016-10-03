b7356f2a3a521adbc960b3e24241b02104f30dc5
David Moneysmith <moneysmith777@gmail.com>
2016-10-03 08:39:55 -0700

    Added images to the backgrounds of the carousels

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -4,6 +4,15 @@ import {Link} from 'react-router';
 export default class Carousel extends Component {
 	componentDidMount() {
 		setInterval(function(){
+			let body = $('body');
+			if (body.hasClass('background1')) {
+
+			} else if (body.hasClass('background2')) {
+
+			} else if (body.hasClass('background3')) {
+
+			}
+
 		  $('.carousel').carousel('next');
 		}, 5000);
 		$('.carousel.carousel-slider').carousel({full_width: true});
@@ -18,15 +27,15 @@ export default class Carousel extends Component {
 			  		Login/Signup
 			  	</Link>
 			  </div>
-			  <div className="carousel-item" href="#one!">
+			  <div className="carousel-item carousel1" href="#one!">
 			    <h2>Education</h2>
 			    <p>Attend or teach a class</p>
 			  </div>
-			  <div className="carousel-item" href="#two!">
+			  <div className="carousel-item carousel2" href="#two!">
 			    <h2>Entertainment</h2>
 			    <p>Stream yourself to friends</p>
 			  </div>
-			  <div className="carousel-item" href="#three!">
+			  <div className="carousel-item carousel3" href="#three!">
 			    <h2>Everyone</h2>
 			    <p>Do you stream?</p>
 			  </div>
